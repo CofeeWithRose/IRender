6177c8c74b236e1de0d2db6005c580246de40c96
grape <grapepeng@tencent.com>
2022-06-09 09:42:22 +0800

    fix error

--- src/core/index.ts
+++ src/core/index.ts
@@ -138,6 +138,7 @@ export class IRender {
         this.gl.linkProgram(program)
 
         console.log('getProgramInfoLog:', this.gl.getProgramInfoLog(program));
+        this.gl.useProgram(program)
         this.gl.enable(this.gl.BLEND)
         this.gl.blendFuncSeparate(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA, this.gl.ONE, this.gl.ONE_MINUS_SRC_ALPHA);
 
@@ -172,7 +173,6 @@ export class IRender {
           updateOffset: this.updateOffset,
           textureManager: this.textureManager,
       }
-        this.gl.useProgram(program)
     }
 
 
